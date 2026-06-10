edb07923dd8d32bd05b76fbee4354f0a09add3ce
prontaraza-hash <prontaraza@gmail.com>
2026-06-10 21:44:07 +0700

    Update backend-server.js

--- backend-server.js
+++ backend-server.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
+oose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
     useUnifiedTopology: true
     }).catch(err => console.log('DB Error:', err));
